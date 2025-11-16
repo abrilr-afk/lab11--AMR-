f58c5d5161f65a6fe71f5c3be8b2db6413963a3d
abril <abril.rodriguez@ufl.edu>
2025-11-16 18:06:29 -0500

    git commit -m "modified calculator p1

--- calculator.py
+++ calculator.py
@@ -6,6 +6,24 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return b / a
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("Base of logarithm must be greater than 0 and not equal to 1.")
+
+def exponent(a, b):
+    return a ** b
 
 
