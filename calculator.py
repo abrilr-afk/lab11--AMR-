e04cec67504d02cbfe034f1fe23bec431597ae03
abril <abril.rodriguez@ufl.edu>
2025-11-16 19:48:14 -0500

    git push

--- calculator.py
+++ calculator.py
@@ -29,7 +29,7 @@ def subtract(a,b):
 def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
     return b / a
